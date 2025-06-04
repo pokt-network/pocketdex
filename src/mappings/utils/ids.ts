d198364568c6963c04d25ed3335311d3264baf1b
Alan Rojas <alan2rm7@gmail.com>
2025-06-04 09:03:14 -0400

    removed blockId from getParamId

--- src/mappings/utils/ids.ts
+++ src/mappings/utils/ids.ts
@@ -37,7 +37,7 @@ export function getBalanceId(address: string, denom: string): string {
 
 // Returns the id of the param entity using the namespace, key, blockId passed.
 export function getParamId(ns: string, key: string): string {
-  return `${ns}-${key}-${blockId.toString()}`;
+  return `${ns}-${key}`;
 }
 
 // Returns the id of the entity that establishes the relationship between the Gateway that the app is being delegated to.
